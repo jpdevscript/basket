808320450d615df2f8d07cff87eecf523422e29a
Jignesh <jp.devscript@gmail.com>
2019-07-21 20:35:13 -0400

    apply changes as requirement

--- src/components/Basket/reducer.js
+++ src/components/Basket/reducer.js
@@ -1,31 +1,33 @@
 import createReducer from "../../utils/createReducer";
 import { types, removeItem, selectItem } from './constants';
-import { 
-  getFilterList, 
-  getTransferList, 
-  getNewItem, 
-  getFilterListAfterDeleteOperation, 
-  checkWithCheckboxIdForList } from './helper';
-import { 
-  mergeStateForSetCheckboxIdsForList, 
-  mergeStateForCaptureAddInputChanges, 
-  mergeStateForMoveItemsToList, 
-  mergeStateForAddItemIntoList, 
-  mergeStateForRemoveItemFromList } from './reducerHelper';
+import {
+  getFilterList,
+  getTransferList,
+  getNewItem,
+  getFilterListAfterDeleteOperation,
+  checkWithCheckboxIdForList
+} from './helper';
+import {
+  mergeStateForSetCheckboxIdsForList,
+  mergeStateForCaptureAddInputChanges,
+  mergeStateForMoveItemsToList,
+  mergeStateForAddItemIntoList,
+  mergeStateForRemoveItemFromList
+} from './reducerHelper';
 
 const Immutable = require("seamless-immutable").static;
 
 const initialState = Immutable.from({
-    list1: [],
-    list2: [],
-    checkboxIdsForList1: [],
-    checkboxIdsForList2: [],
-    list1AddValue: '',
-    list2AddValue: ''
+  list1: [],
+  list2: [],
+  checkboxIdsForList1: [],
+  checkboxIdsForList2: [],
+  list1AddValue: '',
+  list2AddValue: ''
 });
 
 const setListData = (state, { data }) => {
-  return Immutable.merge (state, {list1: [...data.lists.list1], list2: [...data.lists.list2]});
+  return Immutable.merge(state, { list1: [...data.lists.list1], list2: [...data.lists.list2] });
 }
 
 const setCheckboxIdsForList = (state, { fromList, id }) => {
@@ -38,10 +40,10 @@ const captureAddInputChanges = (state, { fromList, value }) => {
   return mergeStateForCaptureAddInputChanges(state, fromList, value);
 }
 
-const moveItemsToList = (state, {fromList}) => {
+const moveItemsToList = (state, { fromList }) => {
   const transferedList = getTransferList(state, fromList);
   const filteredList = getFilterList(state, fromList);
-  
+
   return mergeStateForMoveItemsToList(state, fromList, transferedList, filteredList);
 }
 
@@ -49,7 +51,7 @@ const addItemIntoList = (state, { fromList }) => {
   const newItem = getNewItem(state, fromList);
   if (!newItem.value.trim()) {
     return state;
-  } 
+  }
 
   return mergeStateForAddItemIntoList(state, fromList, newItem);
 }
@@ -64,10 +66,10 @@ const removeItemFromList = (state, { fromList, id }) => {
 const handlers = {
   [types.RECEIVED_LIST_DATA]: setListData,
   [types.HANDLE_CHANGE_EVENT_FOR_ITEM_CHECKBOX]: setCheckboxIdsForList,
-  [types.HANDLE_CHANGE_EVENT_FOR_ADD_INPUT_TYPES]: captureAddInputChanges,
-  [types.CLICK_TRANSFER_BUTTON_FOR_LIST]: moveItemsToList,
-  [types.CLICK_ADD_BUTTON_INSERT_INTO_LIST]: addItemIntoList,
-  [types.CLICK_DELETE_BUTTON_FOR_ITEM]: removeItemFromList,
+  [types.HANDLE_CHANGE_EVENT_FOR_ADD_ITEM]: captureAddInputChanges,
+  [types.HANDLE_TRANSFER_ITEM_FOR_LIST]: moveItemsToList,
+  [types.HANDLE_ADD_ITEM_FOR_LIST]: addItemIntoList,
+  [types.HANDLE_DELETE_ITEM_FOR_LIST]: removeItemFromList,
 }
 
 export default createReducer(initialState, handlers);
